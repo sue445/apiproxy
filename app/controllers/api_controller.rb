767001c3e938aef6f1992b51a7fa0c1ba725a2d1
sue445 <sue445@sue445.net>
2012-11-11 00:28:09 +0900

    add content type

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ApplicationController
   def doorkeeper
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
     render :text => `curl http://api.doorkeeper.jp/events?q=#{params[:q]}&page=#{params[:page]}`
   end
 end
