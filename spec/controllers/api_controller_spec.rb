55d0b84891f416f56abba41d0b28f639e645e4a7
sue445 <sue445@sue445.net>
2012-11-11 00:11:05 +0900

    add red test

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -3,13 +3,29 @@ require 'spec_helper'
 describe ApiController do
 
   describe "GET 'doorkeeper'" do
-    before do
-      get 'doorkeeper'
-    end
-
     it "returns http success" do
+      get 'doorkeeper'
       response.should be_success
     end
+
+    describe "return" do
+      let(:args)do
+        {
+            :q => "github",
+            :page => 1,
+        }
+      end
+
+      before do
+        get 'doorkeeper', args
+        @ret = JSON.parse(response.body)
+      end
+
+      it "json array" do
+        @ret.class.should == Array
+      end
+    end
+
   end
 
 end
